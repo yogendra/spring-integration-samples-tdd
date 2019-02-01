c5495af0c0b6b1744ef8e45e429d320b4154991a
Yogendra Rampuria (Yogi) <yrampuria@pivotal.io>
2019-02-02 00:47:06 +0800

    Initial project commit

---
+++ src/main/java/me/yogendra/samples/springintegrationsamples/SpringIntegrationSamplesTDDApplication.java
@@ -0,0 +1,14 @@
+package me.yogendra.samples.springintegrationsamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIntegrationSamplesTDDApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringIntegrationSamplesTDDApplication.class, args);
+	}
+
+}
+
